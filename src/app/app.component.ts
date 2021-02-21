2e243e2ae56cc32400016f3bc6d6ae04c0158faf
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-02-21 13:26:16 -0300

    Criando serviço e primeira requisição

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-client';
+export class AppComponent implements OnInit{
+
+  constructor(
+    private productsService: ProductsService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.getProducts().subscribe(prods => console.log(prods));
+  }
 }
